bf83f95f3f2cb3ddc1631c35b06120d855699efd
kruser <kruser@yandex.ru>
2023-01-30 21:38:46 +0500

    add frontend

--- where_to_go/settings.py
+++ where_to_go/settings.py
@@ -109,7 +109,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'ru-ru'
 
-TIME_ZONE = 'SECRET_KEY'
+TIME_ZONE = 'Europe/Moscow'
 
 USE_I18N = True
 
@@ -122,7 +122,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+STATICFILES_DIRS = ('./static/',)
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
