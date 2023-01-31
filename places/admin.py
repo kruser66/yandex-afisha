d6403fed0c725631a20d280ae4f84667623b15e8
kruser <kruser@yandex.ru>
2023-01-31 11:29:52 +0500

    add Excursion model

--- places/admin.py
+++ places/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Excursion
 
-# Register your models here.
+
+@admin.register(Excursion)
+class ExcursionAdmin(admin.ModelAdmin):
+    pass
+
--- places/models.py
+++ places/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-print('Привет')
+
+class Excursion(models.Model):
+
+    title = models.CharField('Название', max_length=200)
+    short_description = models.TextField('Краткое описание')
+    long_description = models.TextField('Полное описание', null=True, blank=True)
+    longitude = models.FloatField('Долгота')
+    latitude = models.FloatField('Широта')
+    
+    def __str__(self):
+        return self.title
