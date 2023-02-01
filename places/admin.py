3247729cf79669171cbbc4da67024a96344210c2
kruser <kruser@yandex.ru>
2023-02-01 23:27:59 +0500

    added inlines Image into Exursion

--- places/admin.py
+++ places/admin.py
@@ -2,9 +2,15 @@ from django.contrib import admin
 from .models import Excursion, Image
 
 
+class ImageInline(admin.TabularInline):
+    model = Image
+    fields = ('image', 'order')
+
 @admin.register(Excursion)
 class ExcursionAdmin(admin.ModelAdmin):
-    pass
+    inlines = [
+        ImageInline
+    ]
 
 @admin.register(Image)
 class ImageAdmin(admin.ModelAdmin):
