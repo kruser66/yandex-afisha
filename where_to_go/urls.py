9b1f65ae044399362bed0c199902c5276b91a0f8
kruser <kruser@yandex.ru>
2023-02-02 14:22:11 +0500

    environ sensitive values

--- where_to_go/urls.py
+++ where_to_go/urls.py
@@ -11,4 +11,5 @@ urlpatterns = [
     path('tinymce/', include('tinymce.urls')),
     path('', views.index, name='index')
 ]
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
