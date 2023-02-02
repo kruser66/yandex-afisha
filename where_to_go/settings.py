1834bff584f6b97b53b40f16b7bf41430fd01be9
kruser <kruser@yandex.ru>
2023-02-02 20:55:37 +0500

    fixed for production

--- where_to_go/settings.py
+++ where_to_go/settings.py
@@ -115,9 +115,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-STATICFILES_DIRS = ('./static/',)
+# STATICFILES_DIRS = ('./static/',)
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
